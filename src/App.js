dd23012d7cedd7b4fda0ee5abd8c167b2e28be1d
marcserpa <serpamarcanthony@gmail.com>
2024-02-26 22:57:46 -0500

    Update 2

--- src/App.js
+++ src/App.js
@@ -2,18 +2,41 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+    const scrollToHabitBuilder = () => {
+        const habitBuilderElement = document.querySelector('.habitBuilder');
+        habitBuilderElement.scrollIntoView({ behavior: 'smooth' });
+    };
+
     return (
         <div className="App">
             <div className="dots">
 
             </div>
-                <header>
-                    <h1>TheCultr;</h1>
-                    <p>The Habit Tracking PDF Builder</p>
-                    <input type="button" onClick="document.getElementById('middle').scrollIntoView('habitBuilder');" />
-                </header>
+            <header>
+                <h1>TheDisciple'n;</h1>
+                <p>The Habit Tracking PDF Builder</p>
+                <button onClick={scrollToHabitBuilder}>&#9660;</button>
+            </header>
             <div className={'habitBuilder'}>
-                <p>Sup</p>
+                <label>Themes:
+                    <input type="radio" id="spring" name="theme"/>
+                    <input type="radio" id="summer" name="theme"/>
+                    <input type="radio" id="fall" name="theme"/>
+                    <input type="radio" id="winter" name="theme"/>
+                </label>
+                <section className={'habitPaper'}>
+                    <div className='dayOne'></div>
+                    <div className='dayTwo'></div>
+                    <div className='dayThree'></div>
+                    <div className='dayFour'></div>
+                    <div className='dayFive'></div>
+                    <div className='daySix'></div>
+                    <div className='daySeven'></div>
+
+                    <div className='dayEight'></div>
+                </section>
+                <p>End</p>
             </div>
         </div>
     );
