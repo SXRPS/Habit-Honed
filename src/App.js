c13293844e79a2c0eebfaf1977fd09ca482bb23a
marcserpa <serpamarcanthony@gmail.com>
2024-02-27 19:02:11 -0500

    Update 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+
 import './App.css';
+
 
 function App() {
 
@@ -8,37 +9,174 @@ function App() {
         habitBuilderElement.scrollIntoView({ behavior: 'smooth' });
     };
 
+
+
+
+
     return (
         <div className="App">
             <div className="dots">
 
             </div>
             <header>
-                <h1>TheDisciple'n;</h1>
-                <p>The Habit Tracking PDF Builder</p>
+                <h1>Disciple'n;</h1>
+                <p>The Habit Tracker PDF Builder</p>
                 <button onClick={scrollToHabitBuilder}>&#9660;</button>
             </header>
-            <div className={'habitBuilder'}>
+            <div className='habitBuilder'>
                 <label>Themes:
+                    <input type="radio" id="default" name="theme"/>
                     <input type="radio" id="spring" name="theme"/>
                     <input type="radio" id="summer" name="theme"/>
                     <input type="radio" id="fall" name="theme"/>
                     <input type="radio" id="winter" name="theme"/>
                 </label>
-                <section className={'habitPaper'}>
-                    <div className='dayOne'></div>
-                    <div className='dayTwo'></div>
-                    <div className='dayThree'></div>
-                    <div className='dayFour'></div>
-                    <div className='dayFive'></div>
-                    <div className='daySix'></div>
-                    <div className='daySeven'></div>
+                <div className='habitPaper'>
+                    <div id='dayOne' className="weekOne">
+                        <h1>Day One:</h1>
+                    </div>
+                    <div id='dayEight' className="weekOne"><h1>Day Eight:</h1></div>
+                    <div id='dayTwo' className="weekOne"><h1>Day Two:</h1></div>
+                    <div id='dayNine' className="weekOne"><h1>Day Nine:</h1></div>
+                    <div id='dayThree' className="weekOne"><h1>Day Three:</h1></div>
+                    <div id='dayTen' className="weekOne"><h1>Day Ten:</h1></div>
+                    <div id='dayFour' className="weekOne"><h1>Day Four:</h1></div>
 
-                    <div className='dayEight'></div>
-                </section>
+                    <div id='dayEleven' className="weekTwo"><h1>Day Eleven:</h1></div>
+                    <div id='dayTwelve' className="weekTwo"><h1>Day Twelve:</h1></div>
+                    <div id='dayFive' className="weekTwo"><h1>Day Five:</h1></div>
+                    <div id='dayThirteen' className="weekTwo"><h1>Day Thirteen:</h1></div>
+                    <div id='daySix' className="weekTwo"><h1>Day Six:</h1></div>
+                    <div id='dayFourteen' className="weekTwo"><h1>Day Fourteen:</h1></div>
+                    <div id='daySeven' className="weekTwo"><h1>Day Seven:</h1></div>
+                </div>
+
                 <p>End</p>
             </div>
-        </div>
+            <div className='buttons'>
+                <div className="rowOne">
+                <div id='circles'>
+                    <button>-</button>
+                    <span className="circled">&#9711;</span>
+                    <button>+</button>
+                </div>
+                <div id='squares'>
+                    <button>-</button>
+                    <span className="squared">&#9744;</span>
+                    <button>+</button>
+                </div>
+                <div id='triangles'>
+                    <button>-</button>
+                    <span className="triangled">&#9651;</span>
+                    <button>+</button>
+                </div>
+            </div>
+                <div className="rowTwo">
+                    <div id='circles'>
+                        <button>-</button>
+                        <span className="circled">&#9711;</span>
+                        <button>+</button>
+                    </div>
+                    <div id='squares'>
+                        <button>-</button>
+                        <span className="squared">&#9744;</span>
+                        <button>+</button>
+                    </div>
+                    <div id='triangles'>
+                        <button>-</button>
+                        <span className="triangled">&#9651;</span>
+                        <button>+</button>
+                    </div>
+                </div>
+                    <div className="rowThree">
+                        <div id='circles'>
+                            <button>-</button>
+                            <span className="circled">&#9711;</span>
+                            <button>+</button>
+                        </div>
+                        <div id='squares'>
+                            <button>-</button>
+                            <span className="squared">&#9744;</span>
+                            <button>+</button>
+                        </div>
+                        <div id='triangles'>
+                            <button>-</button>
+                            <span className="triangled">&#9651;</span>
+                            <button>+</button>
+                        </div>
+                    </div>
+                    <div className="rowFour">
+                        <div id='circles'>
+                            <button>-</button>
+                            <span className="circled">&#9711;</span>
+                            <button>+</button>
+                        </div>
+                        <div id='squares'>
+                            <button>-</button>
+                            <span className="squared">&#9744;</span>
+                            <button>+</button>
+                        </div>
+                        <div id='triangles'>
+                            <button>-</button>
+                            <span className="triangled">&#9651;</span>
+                            <button>+</button>
+                        </div>
+                    </div>
+                    <div className="rowFive">
+                        <div id='circles'>
+                            <button>-</button>
+                            <span className="circled">&#9711;</span>
+                            <button>+</button>
+                        </div>
+                        <div id='squares'>
+                            <button>-</button>
+                            <span className="squared">&#9744;</span>
+                            <button>+</button>
+                        </div>
+                        <div id='triangles'>
+                            <button>-</button>
+                            <span className="triangled">&#9651;</span>
+                            <button>+</button>
+                        </div>
+                    </div>
+                    <div className="rowSix">
+                        <div id='circles'>
+                            <button>-</button>
+                            <span className="circled">&#9711;</span>
+                            <button>+</button>
+                        </div>
+                        <div id='squares'>
+                            <button>-</button>
+                            <span className="squared">&#9744;</span>
+                            <button>+</button>
+                        </div>
+                        <div id='triangles'>
+                            <button>-</button>
+                            <span className="triangled">&#9651;</span>
+                            <button>+</button>
+                        </div>
+                    </div>
+                    <div className="rowSeven">
+                        <div id='circles'>
+                            <button>-</button>
+                            <span className="circled">&#9711;</span>
+                            <button>+</button>
+                        </div>
+                        <div id='squares'>
+                            <button>-</button>
+                            <span className="squared">&#9744;</span>
+                            <button>+</button>
+                        </div>
+                        <div id='triangles'>
+                            <button>-</button>
+                            <span className="triangled">&#9651;</span>
+                            <button>+</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
     );
 }
 
